082da498a9b649b8d1693d47d3164466ba8eb74c
Ruveyda <albayrakruveyda@gmail.com>
2023-02-16 15:41:46 -0800

    First commit

--- server.py
+++ server.py
@@ -5,17 +5,18 @@ app = Flask(__name__)
 def playground():
     return render_template('welcome.html')
 
-# @app.route('/play')
-# def play():
-#     return render_template('index.html', num=3)    
+@app.route('/play')
+def play():
+    return render_template('index.html', num=3,color='aqua')    
 
-# @app.route('/play/<int:num>')
-# def playtimes(num):
-#     return render_template('index.html', num=num)
+@app.route('/play/<int:num>')
+def playtimes(num):
+    return render_template('index.html', num=num, color='aqua')
 
 @app.route('/play/<int:num>/<color>')
 def color(color,num):
-    return render_template('index.html',color=color, num=num)
+    
+    return render_template('index.html', num=num, color=color)
 
 
 
